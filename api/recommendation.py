bf22ba3f94210f9e41f34401ec5620a3332db9ce
imoon <imoon@tricycle.co.kr>
2024-06-21 16:51:16 +0900

    가격포맷

--- api/recommendation.py
+++ api/recommendation.py
@@ -45,7 +45,7 @@ try:
         i=0
         for recommend in recommend_list:
             try:
-                recognition_result_container[i%4].image(recommend["appPrdImgUrl"], caption= str(recommend["prdNo"]) + " | "+ str(format(recommend["dcPrcApp"], ',')))
+                recognition_result_container[i%4].image(recommend["appPrdImgUrl"], caption= str(recommend["prdNo"]) + " | "+ str(format(recommend["dcPrcApp"], ',')) + "원")
                 i=i+1
             except Exception as ex:
                 st.text(ex)
